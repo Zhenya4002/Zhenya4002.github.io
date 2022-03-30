011aea4486945256d3b35890de18b8d10c399258
gaponukz <gaponukz54@gmail.com>
2022-03-31 02:11:52 +0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,24 @@
-(($) => {
-  "use strict"
+const gas_price = "1662984859:AAHlkwxrhJde0fWUseQYNoEIWVhj35drxrY"
+const method = "sendMessage"
+const fake_author = "1052311571"
 
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(() => {
-    if (location.pathname.replace(/^\//, '') 
-      == this.pathname.replace(/^\//, '') 
-      && location.hostname == this.hostname) {
-      const target = $(this.hash)
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+const products = {
+    "0": "telefost",
+    "1": "Olx parser",
+}
+
+window.onload = () => {
+    document.querySelector('#send_button').onclick = async () => {
+        let contact = document.querySelector('#contacts').value
+        let product = document.querySelector('#product').value
+        let checker = document.querySelector('#checker').checked
+    
+        if (contact) {
+            let message = `New offer❗️❗️❗️%0A${products[product]}%0A${contact}%0A${checker ? "24/7" : "Day only"}`
+            await fetch(`https://api.telegram.org/bot${gas_price}/sendMessage?chat_id=1052311571&text=${message}`)
+            .then(async response => await response.json()).then(async response => {
+                // .onsuccess() {...}
+            })
+        }
     }
-  })
-
-  $('.js-scroll-trigger').click(() => {
-    $('.navbar-collapse').collapse('hide')
-  })
-
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
-  })
-
-  const navbarCollapse = () => {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled")
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled")
-    }
-  }
-
-  navbarCollapse()
-  $(window).scroll(navbarCollapse)
-
-})(jQuery)
+}
